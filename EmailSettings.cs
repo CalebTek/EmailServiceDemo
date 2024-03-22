9f7fda21a3b3c4ec51ac9c79003c0c9ad170fc5d
CalebTek <walexjackson@gmail.com>
2024-03-22 17:00:48 +0100

    explaining the email settings entity

--- EmailSettings.cs
+++ EmailSettings.cs
@@ -2,10 +2,15 @@
 {
     public class EmailSettings
     {
+        // Connection
         public string SmtpHost { get; set; }
         public int SmtpPort { get; set; }
+
+        // Authentication
         public string SmtpAddress { get; set; }
         public string SmtpPassword { get; set; }
+
+        // Identification
         public string SenderName { get; set; } 
         public string SenderEmail { get; set; }
     }
